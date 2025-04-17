04c1eabf68e95585af5e61d80fb30f46bcbe6b31
Mohammadreza <mrmh13801225@gmail.com>
2025-04-17 19:31:45 +0330

    feat : implement task assignment logic of coordinator using AskForTask rpc

--- CA1/src/mr/coordinator.go
+++ CA1/src/mr/coordinator.go
@@ -62,8 +62,97 @@ func (c *Coordinator) initTask(files []string) {
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) AskForTask(req *MessageSend, reply *MessageReply) error {
+	if req.MsgType != AskForTask {
+		return BadMsgType
+	}
+	if !c.MapSuccess {
+
+		c.muMap.Lock()
+
+		count_map_success := 0
+		for fileName, taskinfo := range c.MapTasks {
+			alloc := false
+
+			if taskinfo.Status == idle || taskinfo.Status == failed {
+				alloc = true
+			} else if taskinfo.Status == running {
+				curTime := time.Now().Unix()
+				if curTime-taskinfo.StartTime > 10 {
+					taskinfo.StartTime = curTime
+					alloc = true
+				}
+			} else {
+				count_map_success++
+			}
+
+			if alloc {
+				reply.MsgType = MapTaskAlloc
+				reply.TaskName = fileName
+				reply.NReduce = c.NReduce
+				reply.TaskID = taskinfo.TaskId
+
+
+				taskinfo.Status = running
+				taskinfo.StartTime = time.Now().Unix()
+				c.muMap.Unlock()
+				return nil
+			}
+		}
+
+		c.muMap.Unlock()
+
+		if count_map_success < len(c.MapTasks) {
+			reply.MsgType = Wait
+			return nil
+		} else {
+			c.MapSuccess = true
+		}
+	}
+
+	if !c.ReduceSuccess {
+		c.muReduce.Lock()
+
+		count_reduce_success := 0
+		for idx, taskinfo := range c.ReduceTasks {
+			alloc := false
+			if taskinfo.Status == idle || taskinfo.Status == failed {
+				alloc = true
+			} else if taskinfo.Status == running {
+				curTime := time.Now().Unix()
+				if curTime-taskinfo.StartTime > 10 {
+					taskinfo.StartTime = curTime
+					alloc = true
+				}
+			} else {
+				count_reduce_success++
+			}
+
+			if alloc {
+				reply.MsgType = ReduceTaskAlloc
+				reply.TaskID = idx
+
+
+				taskinfo.Status = running
+				taskinfo.StartTime = time.Now().Unix()
+
+				c.muReduce.Unlock()
+				return nil
+			}
+		}
+
+		c.muReduce.Unlock()
+
+		if count_reduce_success < len(c.ReduceTasks) {
+			reply.MsgType = Wait
+			return nil
+		} else {
+			c.ReduceSuccess = true
+		}
+	}
+
+	reply.MsgType = Shutdown
+
 	return nil
 }
 

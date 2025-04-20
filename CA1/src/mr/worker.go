b6b1802a54478fe5d40e12e75de3aa54f10f21b8
Mohammadreza <mrmh13801225@gmail.com>
2025-04-20 14:29:40 +0330

    feat : implement a sorting mechanisem by key

--- CA1/src/mr/worker.go
+++ CA1/src/mr/worker.go
@@ -1,9 +1,17 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io"
+	"io/ioutil"
+	"net/rpc"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 
 
 //
@@ -13,6 +21,13 @@ type KeyValue struct {
 	Key   string
 	Value string
 }
+
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // use ihash(key) % NReduce to choose the reduce

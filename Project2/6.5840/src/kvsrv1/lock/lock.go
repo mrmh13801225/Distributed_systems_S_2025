d7bd0b5064e5d7fc6722d7f56d608f250ce075e9
Mahdi <hitking2007@gmail.com>
2025-05-26 19:23:41 +0330

    Mahdi: complete lock logic

--- Project2/6.5840/src/kvsrv1/lock/lock.go
+++ Project2/6.5840/src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -9,8 +12,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
-	// You may add code here
+	ck      kvtest.IKVClerk
+	key     string
+	ownerID string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -20,14 +24,51 @@ type Lock struct {
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
-	// You may add code here
+	lk.key = l
+	lk.ownerID = kvtest.RandValue(8)
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	for {
+		client, version, err := lk.ck.Get(lk.key)
+		if err != rpc.OK {
+			err = lk.ck.Put(lk.key, lk.ownerID, 0)
+			if err != rpc.OK {
+				continue
+			}
+			return
+		} else {
+			if lk.ownerID == client {
+				return
+			} else if client == "" {
+				err = lk.ck.Put(lk.key, lk.ownerID, version)
+				if err != rpc.OK {
+					continue
+				}
+				return
+			} else {
+				time.Sleep(10 * time.Millisecond)
+			}
+		}
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	for {
+		client, version, err := lk.ck.Get(lk.key)
+		if err != rpc.OK {
+			return
+		}
+
+		if client == lk.ownerID {
+			err = lk.ck.Put(lk.key, "", version)
+			if err != rpc.OK {
+				continue
+			}
+			return
+		} else {
+			return
+		}
+	}
 }
